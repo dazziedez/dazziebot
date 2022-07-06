45e2eae9c857f63b7ef8e8fe6253b6a8cae8cc26
Dazzie <79106393+dazziedez@users.noreply.github.com>
2022-07-06 14:21:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,38 +3,18 @@ from discord.ext import commands
 import os
 import asyncio
 import sys
+import time
 
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix=".", intents=intents)
 client.remove_command("help")
 
+
 def restart_bot():
     os.execv(sys.executable, ['python'] + sys.argv)
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-    await ctx.reply(f'{extension} successfully loaded')
-    await print(f'🔵 {extension} successfully loaded')
 
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    await ctx.reply(f'{extension} successfully unloaded')
-    await print(f'🔵 {extension} successfully unloaded')
-
-
-for filename in os.listdir('./cogs'):
-  if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
-    print(f'🔵 "{filename[:-3]}" successfully loaded')
-    
-  else:
-    print(f'🟠 "{filename[:-3]}" could not be loaded')
-
-
-    
 @client.command(aliases=["reboot", "reload"])
 @commands.has_permissions(administrator=True)
 async def restart(ctx):
@@ -46,14 +26,41 @@ async def restart(ctx):
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Activity(
-        type=discord.ActivityType.watching, name="you"))
-    print(f"🟢 Logged in as {client.user}")
+        type=discord.ActivityType.watching, name="you - .gg/bottle"))
+    print(f"🟢 successfully logged in as {client.user}")
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'events.{extension}')
+    await ctx.reply(f'{extension} successfully loaded')
+    await print(f'🔵 {extension} successfully loaded')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'events.{extension}')
+    await ctx.reply(f'{extension} successfully unloaded')
+    await print(f'🔵 {extension} successfully unloaded')
 
 
+for filename in os.listdir('./events'):
+  if filename.endswith('.py'):
+    client.load_extension(f'events.{filename[:-3]}')
+    print(f'🔵 "{filename[:-3]}" successfully loaded')
+    
+  else:
+    print(f'🟠 "{filename[:-3]}" could not be read')
 
 @client.event
 async def on_message(message):
     await client.process_commands(message)
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'Please pass in all requirements - `{error}`')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f"You don't have permission to do that - `{error}`")
 
 
 @client.event
@@ -62,24 +69,40 @@ async def on_member_join(member):
     channel = guild.get_channel(876101520297443388)
     general = guild.get_channel(876085929117352016)
     verify = guild.get_channel(876102248185339925)
-    await channel.send(
-        f'{member.mention} joined this server :O\nRemember, you can\'t leave', delete_after=60)
-    await member.send(
-        f'Welcome to **{guild.name}**, {member.name}!\njust to make things clear, you\'ll die if you leave\nhttps://discord.gg/T5BZayunBB'
-    )
-    await general.send(f'{member.mention} just joined, say hi!')
-    await verify.send(
-        f'welcome {member.mention}!\nPlease react with ✅ above to gain access to the server',
+    log = guild.get_channel(876104888122212413)
+    if time.time() - member.created_at.timestamp() < 2592000:
+        embed = discord.Embed(description=f'You were kicked from `{guild.name}` because your account is too young \n<t:{int(member.created_at.timestamp())}:R> < `30 days ago`')
+        logembed = discord.Embed(description=f'{member.name} was kicked because their account is too young \n<t:{int(member.created_at.timestamp())}:R> < `30 days ago`')
+        await member.send(embed=embed)
+        await guild.kick(member)
+        await log.send(embed=logembed)
+    else:
+        logembed = discord.Embed(title='Member joined', description=f'{member}')
+        logembed.add_field(name="Account Created",
+                    value=f'<t:{int(member.created_at.timestamp())}:R>')
+        logembed.set_thumbnail(url=member.avatar_url)
+        await channel.send(
+        f'{member.mention} joined this server :O\nRemember, you can\'t leave')
+        await member.send(f'Welcome to **{guild.name}**, {member.name}!\njust to make things clear, you\'ll die if you leave\nhttps://discord.gg/T5BZayunBB')
+        await general.send(f'{member.mention} just joined, say hi!',
+                       delete_after=60)
+        await verify.send(f'welcome {member.mention}!\nPlease react with ✅ above to gain access to the server',
         delete_after=10)
+        await log.send(embed=logembed)
 
 
 @client.event
 async def on_member_remove(member):
     guild = client.get_guild(874440438604496976)
     channel = guild.get_channel(879118347088834620)
+    log = guild.get_channel(876104888122212413)
+    logembed = discord.Embed(title='Member left', description=f'{member}')
+    logembed.set_thumbnail(url=member.avatar_url)
+    logembed.add_field(name="Joined At", value=f'<t:{int(member.joined_at.timestamp())}:R>')
     await channel.send(
         f'Someone just left...\nMay {member.name}#{member.discriminator} rest in peace.'
     )
+    await log.send(embed=logembed)
 
 
 snipe_message_content = None
@@ -96,11 +119,15 @@ async def on_message_delete(message):
     global sn_author_name
     global snipe_message_id
 
-    snipe_message_content = message.content
-    snipe_message_author = message.author.id
-    snipe_message_id = message.id
-    sn_author_name = client.get_user(snipe_message_author)
-    await asyncio.sleep(60)
+    if message.author.id == 879083602082684968:
+        return
+
+    else:
+        snipe_message_content = message.content
+        snipe_message_author = message.author.id
+        snipe_message_id = message.id
+        sn_author_name = client.get_user(snipe_message_author)
+        await asyncio.sleep(60)
 
     if message.id == snipe_message_id:
         snipe_message_author = None
@@ -123,6 +150,12 @@ async def snipe(message):
         embed.set_author(name=f"{sn_author_name}")
         await message.channel.send(embed=embed)
         return
+
+
+@client.command()
+@commands.cooldown(1, 4, commands.BucketType.user)
+async def ping(ctx):
+    await ctx.send(f'{round(client.latency * 1000)}ms')
 
 
 @client.command(aliases=["av", "pfp"])
@@ -153,55 +186,59 @@ async def members(ctx):
 @client.command(pass_context=True, aliases=["clear", "purge"])
 @commands.has_permissions(administrator=True)
 @commands.cooldown(1, 4, commands.BucketType.user)
-async def clean(ctx, limit: int):
-    await ctx.channel.purge(limit=limit + 1)
-    await ctx.send(f'{ctx.author.mention} purged {limit} messages',
+async def clean(ctx, Limit: int):
+    await ctx.channel.purge(limit=Limit + 1)
+    await ctx.send(f'{ctx.author.mention} purged {Limit} messages',
                    delete_after=5)
     await ctx.message.delete()
 
 
 @client.command()
 @commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member, *, reason='Unspecified'):
-    if member.id == ctx.author.id:
+async def ban(ctx, Member: discord.Member, *, reason='Unspecified'):
+    guild = client.get_guild(874440438604496976)
+    if Member.id == ctx.author.id:
         await ctx.send("You cannot ban yourself")
         return
 
-    if member.top_role >= ctx.author.top_role:
+    if Member.top_role >= ctx.author.top_role:
         await ctx.send(f"You can only ban members below your top role")
         return
 
-    embed = discord.Embed(
-        title=f'***Banned `{member}` for `{reason}`***', color=0x38805d)
-    await member.ban(reason=reason)
+    embed = discord.Embed(title=f'***Banned `{Member}` for `{reason}`***',
+                          color=0x38805d)
+    dmbed = discord.Embed(description=f'You were banned from `{guild.name}` for `{reason}`.\nDM a staff/owner if you feel this is unjustified')
+    await Member.send(embed=dmbed)
+    await Member.ban(reason=reason)
     await ctx.send(embed=embed, delete_after=10)
     await ctx.message.delete()
 
 
 @client.command()
 @commands.has_permissions(administrator=True)
-async def unban(ctx, *, member):
+async def unban(ctx, *, Member):
     banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split("#")
+    member_name, member_discriminator = Member.split("#")
 
     for ban_entry in banned_users:
         user = ban_entry.user
 
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
+        if (user.name, user.discriminator) == (member_name,
+                                               member_discriminator):
             await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned `{member}`', delete_after=10)
+            await ctx.send(f'Unbanned `{Member}`', delete_after=10)
             await ctx.message.delete()
             return
 
 
 @client.command(description="Mute command yay!!", aliases=["shut"])
 @commands.has_permissions(manage_messages=True)
-async def mute(ctx, member: discord.Member, *, reason='Unspecified'):
-    if member.id == ctx.author.id:
+async def mute(ctx, Member: discord.Member, *, reason='Unspecified'):
+    if Member.id == ctx.author.id:
         await ctx.send("You cannot mute yourself")
         return
 
-    if member.top_role >= ctx.author.top_role:
+    if Member.top_role >= ctx.author.top_role:
         await ctx.send(f"You can only mute members below your top role")
         return
     guild = ctx.guild
@@ -211,44 +248,109 @@ async def mute(ctx, member: discord.Member, *, reason='Unspecified'):
         mutedRole = await guild.create_role(name="Muted")
 
         for channel in guild.channels:
-            await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
+            await channel.set_permissions(mutedRole,
+                                          speak=False,
+                                          send_messages=False,
+                                          read_message_history=True,
+                                          read_messages=False)
 
-    await member.add_roles(mutedRole, reason=reason)
-    embed = discord.Embed(title=f'Muted `{member}` for `{reason}`')
+    await Member.add_roles(mutedRole, reason=reason)
+    embed = discord.Embed(title=f'Muted `{Member}` for `{reason}`')
     await ctx.send(embed=embed, delete_after=10)
-    await member.send(f"You were muted in `{guild.name}` for `{reason}`. DM a staff/owner if you feel this is unjustified")
+    dmbed = discord.Embed(title=f"You were muted in `{guild.name}` for `{reason}`.\nDM a staff/owner if you feel this is unjustified")
+    await Member.send(embed=dmbed)
     await ctx.message.delete()
 
 
 @client.command(description="Unmute command yay!!")
 @commands.has_permissions(manage_messages=True)
-async def unmute(ctx, member: discord.Member):
+async def unmute(ctx, Member: discord.Member):
     mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
 
-    await member.remove_roles(mutedRole)
-    embed = discord.Embed(title=f'Unmuted `{member}`')
+    await Member.remove_roles(mutedRole)
+    embed = discord.Embed(title=f'Unmuted `{Member}`')
     await ctx.send(embed=embed, delete_after=10)
-    await member.send(f"You were unmuted in `{ctx.guild.name}`")
+    dmbed = discord.Embed(title=f"You were unmuted in `{ctx.guild.name}`")
+    await Member.send(embed=dmbed)
     await ctx.message.delete()
 
 
-@client.command(aliases=["abt", 'whois', 'who', 'ui', 'about'], description="Gets info about the user")
-async def userinfo(ctx, *, member:discord.Member = None):
+@client.command(aliases=["abt", 'whois', 'who', 'ui', 'about'],
+                description="Gets info about the user")
+async def userinfo(ctx, *, member: discord.Member = None):
     if member == None:
-            member = ctx.message.author
+        member = ctx.message.author
 
-    embed=discord.Embed(
-      title=f'{member}'
-      )
+    embed = discord.Embed(title=f'{member}')
     embed.set_thumbnail(url=member.avatar_url)
     embed.add_field(name="Name", value=f'`{member.name}`')
     embed.add_field(name="Nickname", value=f'`{member.nick}`')
     embed.add_field(name="ID", value=f'`{member.id}`')
-    embed.add_field(name="Account Created",value=f'<t:{int(member.created_at.timestamp())}>')
-    embed.add_field(name="Joined",value=f'<t:{int(member.joined_at.timestamp())}>')
+    embed.add_field(name="Account Created",
+                    value=f'<t:{int(member.created_at.timestamp())}:R>')
+    embed.add_field(name="Joined",
+                    value=f'<t:{int(member.joined_at.timestamp())}:R>')
     members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
-    embed.add_field(name="Join Position", value=f'`{str(members.index(member)+1)}`')
+    embed.add_field(name="Join Position",
+                    value=f'`{str(members.index(member)+1)}`')
     await ctx.send(embed=embed)
 
 
-client.run('token')
+@client.command()
+async def help(ctx, admin=None):
+
+    nembed = discord.Embed(title="Command list",
+                           description="Type '.help admin' for admin commands")
+    nembed.add_field(name='.help', value='this command')
+
+    nembed.add_field(
+        name='.who',
+        value='details about this command (abt, whois, who, ui, about)',
+        inline=True)
+
+    nembed.add_field(name='.snipe',
+                     value='see the latest deleted message in chat',
+                     inline=True)
+
+    nembed.add_field(name='.ping', value='bot\'s ping', inline=True)
+
+    nembed.add_field(name='.av',
+                     value='see yours or mentioned\'s avatar',
+                     inline=True)
+
+    nembed.add_field(name='.members',
+                     value='view member count (mc, membercount)',
+                     inline=True)
+
+    nembed.set_footer(text=f"all commands start with the '.' prefix")
+
+    ambed = discord.Embed(
+        title="Admin command list",
+        description="These commands will only work if you have admin perms",
+        color=0xff0000)
+
+    ambed.add_field(name='.ban', value='ban users', inline=True)
+
+    ambed.add_field(name='.unban', value='unban users', inline=True)
+
+    ambed.add_field(name='.mute',
+                    value='mute users, they can still read the chat',
+                    inline=True)
+
+    ambed.add_field(name='.unmute',
+                    value='you guessed it, unmute users',
+                    inline=True)
+
+    ambed.add_field(name='.restart',
+                    value='reload main.py and `events` cog',
+                    inline=True)
+
+    if admin == None:
+        await ctx.message.add_reaction('✅')
+        await ctx.reply(embed=nembed, delete_after=60)
+    elif admin.lower() == 'admin':
+        await ctx.message.add_reaction('✅')
+        await ctx.reply(embed=ambed, delete_after=60)
+
+
+client.run(token)
